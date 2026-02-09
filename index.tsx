8020e9ac5ef9799d0185a57b7b9c202a7962d4ba
MagnumTrain <kadimagomedov0598@gmail.com>
2026-02-09 13:12:58 +0300

    Remove oldv1 and gamenewdesign2 routes

--- index.tsx
+++ index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import AppDesignV2 from './AppDesignV2';
 
 const rootElement = document.getElementById('root');
@@ -8,19 +7,9 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const pathname = window.location.pathname.replace(/\/+$/, '');
-const isGameNewDesign2 = pathname === '/gamenewdesign2';
-const isOldV1 = pathname === '/oldv1';
-
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    {isOldV1 ? (
-      <App />
-    ) : isGameNewDesign2 ? (
-      <AppDesignV2 />
-    ) : (
-      <AppDesignV2 />
-    )}
+    <AppDesignV2 />
   </React.StrictMode>
 );
